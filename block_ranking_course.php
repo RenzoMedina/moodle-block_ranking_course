16d62865d19cf88417099fdbeabfacd4d48adbff
RenzoMedina <medinast30@gmail.com>
2026-03-15 15:36:41 -0300

    Add load date of ranking

--- block_ranking_course.php
+++ block_ranking_course.php
@@ -37,7 +37,7 @@ class block_ranking_course extends block_base {
      * @return stdClass The block contents.
      */
     public function get_content() {
-        global $OUTPUT, $CFG;
+        global $OUTPUT, $CFG, $COURSE, $USER, $DB;
         require_once($CFG->dirroot . '/lib/gradelib.php');
         if ($this->content !== null) {
             return $this->content;
@@ -56,7 +56,57 @@ class block_ranking_course extends block_base {
         if (!empty($this->config->text)) {
             $this->content->text = $this->config->text;
         } else {
-            $this->content->text = $OUTPUT->render_from_template('block_ranking_course/main', []);
+            $courseitem = grade_item::fetch_course_item($COURSE->id);
+            $grademax = (float)$courseitem->grademax;
+            $ranking = [];
+            $sql = "SELECT gg.userid, gg.finalgrade
+                    FROM {grade_grades} gg
+                    WHERE gg.itemid = :itemid
+                    AND gg.finalgrade IS NOT NULL
+                    ORDER BY gg.finalgrade DESC";
+
+            $allgrades = $DB->get_records_sql($sql, ['itemid' => $courseitem->id]);
+            foreach ($allgrades as $grade) {
+                $student = $DB->get_record('user', ['id' => $grade->userid], 'firstname, lastname');
+                $percentage = ($grademax > 0) ? round(($grade->finalgrade / $grademax) * 100, 2) : 0;
+                $ranking[] = [
+                    'userid'   => $grade->userid,
+                    'fullname' => fullname($student),
+                    'rank' => $percentage . '%',
+                ];
+            }
+            $context = context_course::instance($COURSE->id);
+            $isteacher = has_capability('moodle/grade:viewall', $context);
+            $isadmin = is_siteadmin($USER->id);
+            $showusers = !empty($this->config->activeusers) ? $this->config->activeusers : 0;
+            $beststudents = array_slice($ranking, 0, 5);
+            $medals = ['🥇' , '🥈', '🥉', '🎖️', '🏅'];
+            foreach ($beststudents as $index => $student) {
+                if ($index > 0  && $beststudents[$index]['rank'] === $beststudents[$index - 1]['rank']) {
+                    $position = $beststudents[$index - 1]['position'];
+                } else {
+                    $position = $index + 1;
+                }
+                $medal = $medals[$index] ?? ($position . 'º');
+                $beststudents[$index]['rank']          = $position . ' ' . $medal;
+                $beststudents[$index]['position']      = $position;
+                $beststudents[$index]['iscurrentuser'] = $iscurrentuser;
+                if ($isteacher || $isadmin) {
+                    $beststudents[$index]['showname'] = true;
+                } else if ($showusers) {
+                    $beststudents[$index]['showname'] = true;
+                } else {
+                    $beststudents[$index]['showname'] = $iscurrentuser;
+                    if (!$iscurrentuser) {
+                        $beststudents[$index]['fullname'] = get_string('hiddenuser', 'block_ranking_course');
+                    }
+                }
+            }
+
+            $templatedata = [
+                'users' => !empty($beststudents) ? $beststudents : [],
+            ];
+            $this->content->text = $OUTPUT->render_from_template('block_ranking_course/main', $templatedata);
         }
 
         return $this->content;
